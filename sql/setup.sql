6fb7b181c6adb875cb49085d14547a256270c1cc
Davide Teofilo <davide.teofilo@mail.polimi.it>
2020-12-15 17:14:09 +0100

    Database part

---
+++ sql/setup.sql
@@ -0,0 +1,10 @@
+drop user if exists green;
+drop schema if exists green;
+
+create user green identified by 'password';
+create schema green;
+
+grant all privileges on green.* to green;
+grant alter routine on green.* to green;
+
+use green;
