2d02b6550b63b152fc7159723f30a8bba8771509
MicheleCoppa <michele.coppa@outlook.com>
2020-12-15 14:56:35 +0100

    Servlet

---
+++ src/main/java/green/Login.java
@@ -0,0 +1,39 @@
+package green;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+		   @Override
+		    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		    		throws ServletException, IOException {
+		        String user = request.getParameter("user");
+		        String password = request.getParameter("password");
+// TODO controllo user  password
+		        request.setAttribute("user", user);
+
+		        String url;
+		        if (user == null || password == null || user.isBlank() || password.isBlank()) {
+		            url = "/unknown.jsp";
+		        } else {
+		            url = "/logged.jsp";
+		        }
+		        request.getRequestDispatcher(url).forward(request, response);
+		
+	}
+
+		   protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+			   
+			   doGet(request, response);
+}
+}
---
+++ src/main/webapp/logged.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Can't login</title>
+</head>
+<body>
+<h1>Login for "${user}" rejected</h1>
+	<p>
+ 		Back <a href="/Green376/index.html">home</a>
+	</p>
+
+</body>
+</html>
