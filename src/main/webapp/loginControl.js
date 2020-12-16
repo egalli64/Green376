f7d8f84421e3b2f232354276b9bec0de9341030a
Luigi Leone <luigileone1993.01@gmail.com>
2020-12-16 14:16:59 +0100

    controllo login e javascript

---
+++ src/main/webapp/loginControl.js
@@ -0,0 +1,13 @@
+document.getElementById('frm1').onsubmit = () => {
+
+	if (document.getElementById("inputUser").value.length < 6) {
+		alert("l'username deve contenere almeno sei caratteri");
+		document.getElementById("inputUser").focus();
+		return false;
+	} else if (document.getElementById("inputPassword").value.length < 8) {
+		alert("La password deve contenere almeno 8 caratteri");
+		document.getElementById("inputPassword").focus();
+		return false;
+	}
+	return true;
+};
