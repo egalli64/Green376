e0270cbd1f0182063c7de3052ed758c78e1c66cd
yuri kita <yuri-kita@live.it>
2020-12-21 17:09:19 +0100

    data.java e result.jsp

--- src/main/java/data/RecordingDao.java
+++ src/main/java/data/RecordingDao.java
@@ -1,83 +1,44 @@
 package data;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RecordingDao implements Dao<Recording> {
+import green.JpaUtil;
+
+
+public class RecordingDao {
 	private static final Logger LOG = LoggerFactory.getLogger(RecordingDao.class);
 
-	private static final String GET_BY_STATION = "SELECT * FROM recordings WHERE station = ?;";
-	private static final String GET_ALL = "SELECT * FROM recordings;";
+	public List<Recording> readByStation(int station_id) {
+        EntityManager em = null;
 
-	@Override
-	public List<Recording> getAll() {
-		List<Recording> results = new ArrayList<Recording>();
-		try (Connection conn = Connector.getConnection();
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery(GET_ALL)) {
-			while (rs.next()) {
-				Recording rec = new Recording(rs.getInt(1), rs.getDate(2), rs.getDouble(3), rs.getDouble(4),
-						rs.getDouble(5), rs.getDouble(6), rs.getDouble(7), rs.getDouble(8), rs.getDouble(9),
-						rs.getDouble(10), rs.getInt(11));
-				results.add(rec);
-			}
-		} catch (SQLException se) {
-			LOG.error("Can't get all recordings", se);
-		}
-		return results;
-	}
-
-//	@Override
-//	public Recording getByStation(int n) {
-//		
-//		Recording result = new Recording();
-//		try(Connection conn = Connector.getConnection();
-//				PreparedStatement stmt = conn.prepareStatement(GET_BY_STATION);
-//				stmt.setInt(1, n);
-//				ResultSet rs = stmt.executeQuery()){
-//			result.setRecoding_id(rs.getInt(1));
-//			result.setDate(rs.getDate(2));
-//			//result.
-////			 rs.getDouble(3), rs.getDouble(4),
-////					rs.getDouble(5), rs.getDouble(6), rs.getDouble(7), rs.getDouble(8), rs.getDouble(9),
-////					rs.getDouble(10), rs.getInt(11));
-//			
-//		} catch (SQLException se) {
-//			LOG.error("Can't get the recording", se);
-//		}
-//		
-//		return result;
-//	}
-
-	@Override
-	public void save(Recording t) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void update(Recording t) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void delete(long id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Recording getByStation(int n) {
-		// TODO Auto-generated method stub
-		return null;
+        try {
+            em = JpaUtil.getEntityManager();
+            String jpql = "SELECT e from Recording e where e.station_id = ?1";
+            var query = em.createQuery(jpql, Recording.class);
+            query.setParameter(1,station_id);
+            List<Recording> rs = query.getResultList();
+            return rs;
+ 
+        } catch (Exception ex) {
+            LOG.error("Can't create query: " + ex.getMessage());
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
 	}
 
 }
--- src/main/webapp/logged.jsp
+++ src/main/webapp/logged.jsp
@@ -11,6 +11,23 @@
 	<p>
  		Back <a href="index.html">home</a>
 	</p>
+	
+	
+	<form action="data">
+	<input type="radio" id="1" name="station" value="1" checked>
+	<label for="1">Station 1</label>
+	<input type="radio" id="2" name="station" value="2">
+	<label for="2">Station 2</label>
+	<input type="radio" id="3" name="station" value="3">
+	<label for="3">Station 3</label>
+	<input type="radio" id="4" name="station" value="4">
+	<label for="4">Station 4</label>
+	<input type="radio" id="5" name="station" value="5">
+	<label for="5">Station 5</label>
+	<button type="submit">cerca</button>
+	</form>
+	
+	
 
 </body>
 </html>
